5b3df68f89b77f1b53b5f90a9458173fe3bc3022
chaithanya0808 <113684808+chaithanya0808@users.noreply.github.com>
2025-11-08 13:00:29 +0530

    Create externaltable.sql

---
+++ externaltable.sql
@@ -0,0 +1,27 @@
+
+create master key
+
+--Drop database scoped credential [CRE_SEA-SQL-DBDEVTEST]
+
+Create database scoped credential [CRE_SEA-SQL-DBDEVTEST]
+WITH IDENTITY = 'chaithanya' , secret = 'rduHiv74!@#$'
+
+-- drop external data source [EXT_SEA-SQL-DBDEVTEST]
+
+create external data source [EXT_SEA-SQL-DBDEVTEST] with
+(
+TYPE = RDBMS,
+LOCATION = 'chaithdev.database.windows.net',
+DATABASE_NAME = 'devtest',
+credential = [CRE_SEA-SQL-DBDEVTEST]
+);
+
+--drop external table [dbo].[employee]
+
+create external table employee(id int, name varchar(20), dept_id int)
+with
+(
+data_source = [EXT_SEA-SQL-DBDEVTEST]
+)
+
+select * from employee
